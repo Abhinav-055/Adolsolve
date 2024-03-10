170a5b103b22f3f2bc757bad182b2643c0043eed
Abhinav Anand <140488187+Abhinav-055@users.noreply.github.com>
2024-03-10 12:29:39 +0530

    Add files via upload

---
+++ adol/adol_app/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import PostListView,PostCreateView,PostDetailView,add_comment_to_post
+urlpatterns = [
+    path('',views.home,name="Home"),
+    path('register/', views.register_user, name='register_user'),
+    path('login/', views.login_user, name='login_user'),
+    path('logout/', views.logout_user, name='logout_user'),
+    path('adol_solve/',views.solve_prob,name='solve_problem'),
+    path('post_list', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/comment/', add_comment_to_post, name='add_comment_to_post'),
+    path('tasks/', views.task_list, name='task_list'),
+    path('complete_task/', views.complete_task, name='complete_task'),
+    path('contact/',views.contact,name='Contact')
+    #path('search/', views.search_posts, name='search_posts'),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
